d9818d71975b7a0395f300e0d73cca8e806d835c
Filipe Figueiredo <70453488+Filz0r@users.noreply.github.com>
2022-08-17 17:03:37 +0100

    updated to answer to exercise 7 of part 1

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -10,7 +10,7 @@ const Title = (props) => {
   return <h1>{text}</h1>;
 };
 
-const Content = (props) => {
+const StatisticLine = (props) => {
   const { name, count } = props;
   return (
     <p>
@@ -24,18 +24,27 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [all, setAll] = useState(0);
+  const [average, setAverage] = useState(0);
 
   const addGoodReview = () => {
     setGood(good + 1);
+    setAll(all + 1);
+    setAverage(average + 1);
   };
 
   const addNeutralReview = () => {
+    setAll(all + 1);
     setNeutral(neutral + 1);
   };
 
   const addBadReview = () => {
     setBad(bad + 1);
+    setAll(all + 1);
+    setAverage(average - 1);
   };
+
+  const positivePercent = `${(good * 100) / all}%`;
 
   return (
     <div>
@@ -44,9 +53,12 @@ const App = () => {
       <Button handleClick={addNeutralReview} text='Neutral' />
       <Button handleClick={addBadReview} text='Bad' />
       <Title text='Statistics' />
-      <Content name='good' count={good} />
-      <Content name='neutral' count={neutral} />
-      <Content name='bad' count={bad} />
+      <StatisticLine name='good' count={good} />
+      <StatisticLine name='neutral' count={neutral} />
+      <StatisticLine name='bad' count={bad} />
+      <StatisticLine name='all' count={all} />
+      <StatisticLine name='average' count={average / all} />
+      <StatisticLine name='positive' count={positivePercent} />
     </div>
   );
 };
