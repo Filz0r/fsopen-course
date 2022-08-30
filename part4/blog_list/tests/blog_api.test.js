c772045121fdcf7064c60c47205e68aebbd0c541
Filipe Figueiredo <70453488+Filz0r@users.noreply.github.com>
2022-08-30 01:45:48 +0100

    added solution for exercises 13-14 from part 4

--- part4/blog_list/tests/blog_api.test.js
+++ part4/blog_list/tests/blog_api.test.js
@@ -12,73 +12,114 @@ beforeEach(async () => {
   await Blog.insertMany(helper.initialBlogs);
 });
 
-test('blogs are returned as JSON', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/);
-}, 10000);
+describe('checking if the database is well structured', () => {
+  test('check if the blog objects contain a field called id', async () => {
+    const response = await api.get('/api/blogs');
 
-test('database contains all the blogs from helper function', async () => {
-  const response = await api.get('/api/blogs');
+    expect(response.body[0].id).toBeDefined();
+  }, 10000);
 
-  expect(response.body).toHaveLength(helper.initialBlogs.length);
-}, 10000);
+  test('database contains all the blogs from helper function', async () => {
+    const response = await api.get('/api/blogs');
 
-test('check if the blog objects contain a field called id', async () => {
-  const response = await api.get('/api/blogs');
-
-  expect(response.body[0].id).toBeDefined();
-}, 10000);
-
-test('can add an blog to the db', async () => {
-  const blogToInsert = {
-    author: 'SomeAuthor',
-    title: 'Some Tittle',
-    url: 'https://example.com/this-is-a-sample-url',
-    likes: 0,
-  };
-
-  await api
-    .post('/api/blogs')
-    .send(blogToInsert)
-    .expect(201)
-    .expect('Content-Type', /application\/json/);
-
-  const blogsAtEnd = await helper.blogsInDb();
-  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1);
+    expect(response.body).toHaveLength(helper.initialBlogs.length);
+  }, 10000);
 });
 
-test('check if the likes property defaults to 0 when missing', async () => {
-  const blogToInsert = {
-    author: 'SomeAuthor',
-    title: 'Some Tittle',
-    url: 'https://example.com/this-is-a-sample-url',
-  };
+describe('checking if the API can comunicante with the db properly', () => {
+  test('blogs are returned as JSON', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/);
+  }, 10000);
 
-  const response = await api
-    .post('/api/blogs')
-    .send(blogToInsert)
-    .expect(201)
-    .expect('Content-Type', /application\/json/);
+  test('can add an blog to the db', async () => {
+    const blogToInsert = {
+      author: 'SomeAuthor',
+      title: 'Some Tittle',
+      url: 'https://example.com/this-is-a-sample-url',
+      likes: 0,
+    };
 
-  expect(response.body.likes).toBe(0);
+    await api
+      .post('/api/blogs')
+      .send(blogToInsert)
+      .expect(201)
+      .expect('Content-Type', /application\/json/);
+
+    const blogsAtEnd = await helper.blogsInDb();
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1);
+  });
+
+  test('checking if a blog can be deleted', async () => {
+    const blogsAtStart = await helper.blogsInDb();
+    const blogToDelete = blogsAtStart[0];
+
+    await api.delete(`/api/blogs/${blogToDelete.id}`);
+
+    const blogsAtEnd = await helper.blogsInDb();
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length - 1);
+
+    const blogsAfterDeleted = blogsAtEnd.map((blog) => blog.title);
+    expect(blogsAfterDeleted).not.toContain(blogToDelete.title);
+  });
+
+  test('checking if a blog can be updated', async () => {
+    const blogsAtStart = await helper.blogsInDb();
+    const blogToUpdate = blogsAtStart[0];
+
+    const updatedBlog = { ...blogToUpdate };
+    updatedBlog.likes += 1;
+
+    await api
+      .put(`/api/blogs/${blogToUpdate.id}`)
+      .send(updatedBlog)
+      .expect(200)
+      .expect('Content-Type', /application\/json/);
+
+    const updatedBlogs = await helper.blogsInDb();
+    const blogAfterUpdate = updatedBlogs[0];
+    expect(blogAfterUpdate.likes).toBe(blogToUpdate.likes + 1);
+  });
 });
 
-test("data can't be pushed without an title", async () => {
-  const blogToInsertWithoutTitle = {
-    author: 'SomeAuthor',
-    url: 'https://example.com/this-is-a-sample-url',
-  };
+describe('validating data before adding it to the database', () => {
+  test('check if the likes property defaults to 0 when missing', async () => {
+    const blogToInsert = {
+      author: 'SomeAuthor',
+      title: 'Some Tittle',
+      url: 'https://example.com/this-is-a-sample-url',
+    };
 
-  await api.post('/api/blogs').send(blogToInsertWithoutTitle).expect(400);
+    const response = await api
+      .post('/api/blogs')
+      .send(blogToInsert)
+      .expect(201)
+      .expect('Content-Type', /application\/json/);
+
+    expect(response.body.likes).toBe(0);
+  });
+
+  test("data can't be pushed without an title", async () => {
+    const blogToInsertWithoutTitle = {
+      author: 'SomeAuthor',
+      url: 'https://example.com/this-is-a-sample-url',
+    };
+
+    await api.post('/api/blogs').send(blogToInsertWithoutTitle).expect(400);
+  });
+
+  test("data can't be pushed without an url", async () => {
+    const blogToInsertWithoutUrl = {
+      author: 'SomeAuthor',
+      title: 'SomeTitle',
+    };
+
+    await api.post('/api/blogs').send(blogToInsertWithoutUrl).expect(400);
+  });
 });
 
-test("data can't be pushed without an url", async () => {
-  const blogToInsertWithoutUrl = {
-    author: 'SomeAuthor',
-    title: 'SomeTitle',
-  };
-
-  await api.post('/api/blogs').send(blogToInsertWithoutUrl).expect(400);
+afterAll(() => {
+  mongoose.connection.close();
 });
